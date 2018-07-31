cffacb5ef9e5cd6d2d5f84586d7f1e1447ec27d1
Emmanuel Roubin <emmanuel.roubin@univ-grenoble-alpes.fr>
2018-07-31 13:25:18 +0200

    add full list and sets

--- yata/static/images/items/get_images.py
+++ yata/static/images/items/get_images.py
@@ -5,7 +5,7 @@ from random import randint
 def random_number(length=3):
     return randint(10**(length-1), (10**(length)-1))
     
-fetch = numpy.arange(871,880)
+fetch = numpy.arange(871,1000)
 for i in fetch:
     print( "get image:", i+1 )
     if not os.path.isfile( "{}/large.png".format( i+1 ) ):
