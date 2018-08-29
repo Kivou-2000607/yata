4aae0cd7fbb1486605167a2b8781518d45b5a343
Kivou-2000607 <manu.roubin@free.fr>
2018-08-29 10:03:58 +0200

    catch api server error + typo

--- bazaar/handy.py
+++ bazaar/handy.py
@@ -4,21 +4,23 @@ def apiCall(section, id, selections, key, sub=None):
     try:
         r = requests.get("https://api.torn.com/{}/{}?selections={}&key={}".format(section, id, selections, key))
         r.raise_for_status()
+
+        rjson = r.json()
+
+        if "error" in rjson:  # standard api error
+            err = rjson
+        else:
+            if sub is not None:
+                if sub in rjson:
+                    return rjson[sub]
+                else:  # key not found
+                    err = dict({"error": {"code": "", "error": "key not found... something went wrong..."}})
+            else:
+                return rjson
+
     except requests.exceptions.HTTPError as e:
         print("[apiCall] API HTTPError {}".format(e))
         err = dict({"error": {"code": r.status_code, "error": "{} #blameched".format(r.reason)}})
 
-    rjson = r.json()
-
-    if "error" in rjson:  # standard api error
-        err = rjson
-    else:
-        if sub is not None:
-            if sub in rjson:
-                return rjson[sub]
-            else:  # key not found
-                err = dict({"error": {"code": "", "error": "key not found... something went wrong..."}})
-        else:
-            return rjson
 
     return dict({"apiError": "API error code {}: {}.".format(err["error"]["code"], err["error"]["error"])})
