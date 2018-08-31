49728b4e5b1af8123413579a0456e02cd6d4c7f7
Kivou-2000607 <manu.roubin@free.fr>
2018-08-31 08:50:44 +0200

    try again

--- bazaar/handy.py
+++ bazaar/handy.py
@@ -26,11 +26,12 @@ def apiCall(section, id, selections, key, sub=None):
 
 
 def getTopThreeUpdates(itemUpdates):
-    # print(itemUpdates.discinct())
-    # listItems = [a.item.tName for a in itemUpdates.all()]
-    # distItems = list(set(listItems))
-    # distOccur = [0]*len(distItems)
-    # for i, item in enumerate(distItems):
-    #     distOccur[i] = listItems.count(item)
-    # return sorted(zip(distOccur, distItems), reverse=True)[:3]
-    return False
+    print(itemUpdates.discinct())
+    listItems = [a.item.tName for a in itemUpdates.all()]
+    distItems = list(set(listItems))
+    distOccur = [0]*len(distItems)
+    for i, item in enumerate(distItems):
+        print(item)
+        distOccur[i] = listItems.count(item)
+    return sorted(zip(distOccur, distItems), reverse=True)[:3]
+    #return False
