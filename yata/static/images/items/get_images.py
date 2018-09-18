69d59b1f7ca8ea421ce0d79d1c4f6c5e689cadc1
Kivou <manu.roubin@free.fr>
2018-09-18 21:54:06 +0200

    new images

--- yata/static/images/items/get_images.py
+++ yata/static/images/items/get_images.py
@@ -5,7 +5,7 @@ from random import randint
 def random_number(length=3):
     return randint(10**(length-1), (10**(length)-1))
     
-fetch = numpy.arange(900,1000)
+fetch = numpy.arange(910,998)
 for i in fetch:
     print( "get image:", i+1 )
     if not os.path.isfile( "{}/large.png".format( i+1 ) ):
