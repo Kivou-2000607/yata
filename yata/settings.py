bc3da3baff7c5b2f473144e9cd0ef9631a701924
Kivou <manu.roubin@free.fr>
2018-10-31 15:28:23 +0100

    debug

--- yata/settings.py
+++ yata/settings.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY set in config variables
