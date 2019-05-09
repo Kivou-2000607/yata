23dc07e1aeff297de4ed8335b004154ce5db84f4
Kivou-2000607 <manu.roubin@free.fr>
2019-05-09 14:36:51 +0200

    migration bazaar

--- bazaar/management/commands/scan.py
+++ bazaar/management/commands/scan.py
@@ -20,25 +20,26 @@ class Command(BaseCommand):
             print("[command.bazaar.scan] api error: {}".format(items["apiError"]))
         else:
             for k, v in items.items():
-                # req = Item.objects.filter(tId=int(k))
-                # if len(req) == 0:
-                #     item = Item.create(k, v)
-                #     item.save()
-                # elif len(req) == 1:
-                #     item = req[0]
-                #     item.update(v)
-                #     if item.onMarket:
-                #         item.update_bazaar(key=preference.key)
-                #     item.save()
-                # else:
-                #     print("[command.bazaar.scan]: request found more than one item id", len(req))
-                #     return 0
+                req = Item.objects.filter(tId=int(k))
+                if len(req) == 0:
+                    item = Item.create(k, v)
+                    item.save()
+                elif len(req) == 1:
+                    item = req[0]
+                    item.update(v)
+                    if item.onMarket:
+                        item.update_bazaar(key=preference.key)
+                    item.save()
+                else:
+                    print("[command.bazaar.scan]: request found more than one item id", len(req))
+                    return 0
 
             preference.lastScanTS = int(timezone.now().timestamp())
             preference.save()
 
         # delete bazaar info for custom items refreshed more than 24h ago
-        for item in Item.objects.filter(onMarket=False).filter(lastUpdateTS__lt=(int(timezone.now().timestamp()) - 3600*24)):
+        items = Item.objects.filter(onMarket=False).filter(lastUpdateTS__lt=(int(timezone.now().timestamp()) - 86400))
+        for item in items:
             item.marketdata_set.all().delete()
 
         print("[command.bazaar.scan] end")
