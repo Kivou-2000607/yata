ebc9587efda7a3233e172c81f5c321bb3abf2666
Kivou-2000607 <manu.roubin@free.fr>
2019-05-21 22:36:11 +0200

    commande tree

--- chain/management/commands/tree.py
+++ chain/management/commands/tree.py
@@ -19,30 +19,10 @@ This file is part of yata.
 
 from django.core.management.base import BaseCommand
 
-from django.conf import settings
-
 from chain.models import Faction
-from yata.handy import apiCall
+from chain.functions import factionTree
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-
-bridge = {"Criminality": 0,
-          "Fortitude": 1,
-          "Voracity": 2,
-          "Toleration": 3,
-          "Excursion": 4,
-          "Steadfast": 5,
-          "Aggression": 6,
-          "Suppression": 7,
-        }
-
-posterOpt = dict({})
-
-#
-
+import json
 
 class Command(BaseCommand):
     def handle(self, **options):
@@ -57,24 +37,5 @@ class Command(BaseCommand):
                 break
             factionId = faction.tId
             keyHolder, key = faction.getRadomKey()
-
-            # call for upgrades
-            upgrades = apiCall('faction', factionId, 'upgrades', key, sub='upgrades')
-            if 'apiError' in upgrades:
-                print('[command.chain.tree] api key error: {}'.format((upgrades['apiError'])))
-                break
-
-            faction = apiCall('faction', factionId, 'basic', key)
-            if 'apiError' in faction:
-                print('[command.chain.tree] api key error: {}'.format((faction['apiError'])))
-                break
-
-            # building upgrades tree
-            tree = dict({})
-            for k, upgrade in sorted(upgrades.items(), key=lambda x: x[1]['branchorder'], reverse=False):
-                if upgrade['branch'] != 'Core':
-                    if tree.get(upgrade['branch']) is None:
-                        tree[upgrade['branch']] = dict({})
-                    tree[upgrade['branch']][upgrade['name']] = upgrade
-
-            factionTree(faction, upgrades, tree)
+            if len(json.loads(faction.posterOpt)):
+                factionTree(faction)
