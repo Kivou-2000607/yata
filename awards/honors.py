fde0179c60d2f54f53347c49990b443c9e9ccbd8
Kivou-2000607 <kivou.2000607@gmail.com>
2020-04-15 10:21:42 +0200

    add RNG banner

--- awards/honors.py
+++ awards/honors.py
@@ -49,7 +49,7 @@ d = {
     571: 559214781,  # Chequered Past
     334: 204652023,  # Compulsive
     581: 832763049,  # On Track
-    839: 0,  # RNG
+    839: 172807476,  # RNG
 
     # 1
     171: 228001376,  # Afghanistan
