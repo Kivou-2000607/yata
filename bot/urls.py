69279c62e25d536ff4d3d6cf41fc452658e045a3
Kivou-2000607 <kivou.2000607@gmail.com>
2020-05-10 16:45:47 +0200

    yata link to invite the bot

--- bot/urls.py
+++ bot/urls.py
@@ -1,4 +1,5 @@
 from django.urls import re_path
+from django.shortcuts import redirect
 
 from . import views
 
@@ -9,6 +10,7 @@ urlpatterns = [
     re_path(r'^documentation/$', views.documentation, name='documentation'),
     re_path(r'^host/$', views.host, name='host'),
     re_path(r'^admin/$', views.admin, name='admin'),
+    re_path(r'^invite/$', lambda x: redirect('https://discordapp.com/oauth2/authorize?client_id=623862007434706986&scope=bot&permissions=8'), name="invite"),
 
     re_path(r'^updateId/$', views.updateId, name='updateId'),
     re_path(r'^togglePerm/$', views.togglePerm, name='togglePerm'),
