c8f68621733471c4375e95d1caa2beeece94f102
Kivou [2000607] <kivou.2000607@gmail.com>
2020-09-30 16:46:14 +0200

    Add recommended update time to loot api

--- api/views/loot.py
+++ api/views/loot.py
@@ -28,8 +28,22 @@ from ratelimit.core import get_usage, is_ratelimited
 from yata.handy import tsnow
 from loot.models import NPC
 
-@cache_page(60*5)
-@ratelimit(key='ip', rate='5/m')
+# update level
+# UPDATE_LEVEL = 0  # level 1
+# UPDATE_LEVEL = 30 * 60  # level 2
+# UPDATE_LEVEL = 90 * 60  # level 3
+UPDATE_LEVEL = 210 * 60  # level 4
+# UPDATE_LEVEL = 450 * 60  # level 5
+
+IGNORE_TIME = 30 * 60  # ignore a NPC if he passes the loot level
+IGNORE_TIME = 30 * 60  # ignore a NPC if he passes the loot level
+
+DEFAULT_UPDATE = 60 * 60  # by default next update is in an hour
+
+UPDATE_TIME = 15 * 60  # time elapsed after the loot level to do the update
+
+# @cache_page(60*5)
+# @ratelimit(key='ip', rate='5/m')
 def loot(request):
     try:
 
@@ -38,12 +52,26 @@ def loot(request):
             status = 429
 
         else:
-                payload = {
-                    "hospout": {npc.tId: npc.hospitalTS for npc in NPC.objects.filter(show=True).order_by('tId')},
-                    "timestamp": tsnow()
-                    }
-                status = 200
-                return JsonResponse(payload, status=200)
+            # get time
+            ts = tsnow()
+
+            # get npcs
+            npcs = {npc.tId: npc.hospitalTS for npc in NPC.objects.filter(show=True).order_by('hospitalTS')}
+
+            # by default next update is in an hour
+            next_update = ts + DEFAULT_UPDATE
+            for id, hosp_out in npcs.items():
+                # check if not to be ignored ignored (IGNORE_TIME seconds after loot level)
+                if ts + IGNORE_TIME < hosp_out + UPDATE_LEVEL:
+                    next_update = hosp_out + UPDATE_LEVEL + UPDATE_TIME
+
+            payload = {
+                "hosp_out": npcs,
+                "next_update": next_update
+            }
+
+            status = 200
+            return JsonResponse(payload, status=200)
 
     except BaseException as e:
         payload = {"error": {"code": 1, "error": f'{e}'}}
