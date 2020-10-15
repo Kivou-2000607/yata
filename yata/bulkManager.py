06906a727a6617e7cbd4f34525916c0f21d82610
Decap <jackwetson@gmail.com>
2020-10-15 13:23:21 +0100

    Bulk query manager

---
+++ yata/bulkManager.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from django.apps import apps
+
+
+class BulkCreateManager(object):
+
+    def __init__(self, chunk_size=100):
+        self._create_queues = defaultdict(list)
+        self.chunk_size = chunk_size
+
+    def _commit(self, model_class):
+        model_key = model_class._meta.label
+        model_class.objects.bulk_create(self._create_queues[model_key])
+        self._create_queues[model_key] = []
+
+    def add(self, obj):
+        model_class = type(obj)
+        model_key = model_class._meta.label
+        self._create_queues[model_key].append(obj)
+        if len(self._create_queues[model_key]) >= self.chunk_size:
+            self._commit(model_class)
+
+    def done(self):
+        for model_name, objs in self._create_queues.items():
+            if len(objs) > 0:
+                self._commit(apps.get_model(model_name))
