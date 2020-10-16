d3ea1881859453b55d8e0f028e947c75f28913be
Kivou [2000607] <kivou.2000607@gmail.com>
2020-10-17 00:02:43 +0200

    fix url typo for debug

--- yata/urls.py
+++ yata/urls.py
@@ -66,4 +66,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += tatic(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
