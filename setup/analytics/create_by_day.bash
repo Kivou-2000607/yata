a9b8b32af16b293bd12d95ec1bf05d3339cf20c2
Kivou [2000607] <kivou.2000607@gmail.com>
2020-11-10 00:43:54 +0100

    adapt analytics to crontabs

--- setup/analytics/create_by_day.bash
+++ setup/analytics/create_by_day.bash
@@ -1,82 +1,61 @@
 #!/bin/bash
 
-    
-y=$(date "+%Y" -d "4 day ago")
-t=$(date "+%Y-%m" -d "4 day ago")
-echo $y
-echo $t
+# get today
+y=$(date "+%Y")
+m=$(date "+%m")
+d=$(date "+%d")
 
-for f in logs/$y/http-$t-*.log.gz; do
-    name=$(echo "$f" | sed -n 's/.*\([0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\).*/\1/p')
-    echo "File -> $f -> $name"
-    if test -f "../../yata/media/analytics/api-v1_$name.json"; then
-        echo "api-v1_$name.json exists"
-    else
-	echo "api-v1_$name.json create"
-        zcat $f | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/api-v1_$name.json -p goaccess_public.conf --date-spec=hr --no-progress
-    fi
-    if test -f "../../yata/media/analytics/web_$name.json"; then
-        echo "web-v1_$name.json exists"
-    else
-	echo "web-v1_$name.json create"
-        zcat $f | sed '/static\|loot\/timings\|abroad\/export\|abroad\/import\|targets\/import\|targets\/export\|\/api\|\/share/d' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/web_$name.json -p goaccess_public.conf --date-spec=hr --no-progress
-    fi
+yp=$(date "+%Y" -d "4 days ago")
+mp=$(date "+%m" -d "4 days ago")
+dp=$(date "+%d" -d "4 days ago")
+
+# remove todays log
+sections=("api-v1 web")
+for section in $sections; do
+    rm -vf $file "../../yata/media/analytics/${section}_$y-$m-$d."*
+    rm -vf $file ../../yata/media/analytics/${section}_$y-$m-$(date "+%d" -d "1 days ago").*
 done
 
-for f in logs/$y/http-$t-*.log; do
-    name=$(echo "$f" | sed -n 's/.*\([0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\).*/\1/p')
-    echo "File -> $f -> $name"
-    if test -f "../../yata/media/analytics/api-v1_$name.json"; then
-        echo "api-v1_$name.json exists"
-    else
-	echo "api-v1_$name.json create"
-        cat $f | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/api-v1_$name.json -p goaccess_public.conf --date-spec=hr --no-progress
-    fi
-    if test -f "../../yata/media/analytics/web_$name.json"; then
-        echo "web-v1_$name.json exists"
-    else
-	echo "web-v1_$name.json create"
-        cat $f | sed '/static\|loot\/timings\|abroad\/export\|abroad\/import\|targets\/import\|targets\/export\|\/api\|\/share/d' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/web_$name.json -p goaccess_public.conf --date-spec=hr --no-progress
-    fi
-done
-
-
-y2=$(date "+%Y")
-t2=$(date "+%Y-%m")
-echo $y2
-echo $t2
-if [ $t2 != $t ]; then
-    for f in logs/$y2/http-$t2-*.log.gz; do
-	name=$(echo "$f" | sed -n 's/.*\([0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\).*/\1/p')
-	echo "File -> $f -> $name"
-	if test -f "../../yata/media/analytics/api-v1_$name.json"; then
-            echo "api-v1_$name.json exists"
-	else
-	    echo "api-v1_$name.json create"
-            zcat $f | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/api-v1_$name.json -p goaccess_public.conf --date-spec=hr --no-progress
-	fi
-	if test -f "../../yata/media/analytics/web_$name.json"; then
-            echo "web-v1_$name.json exists"
-	else
-	    echo "web-v1_$name.json create"
-            zcat $f | sed '/static\|loot\/timings\|abroad\/export\|abroad\/import\|targets\/import\|targets\/export\|\/api\|\/share/d' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/web_$name.json -p goaccess_public.conf --date-spec=hr --no-progress
-	fi
-    done
-
-    for f in logs/$y2/http-$t2-*.log; do
-	name=$(echo "$f" | sed -n 's/.*\([0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\).*/\1/p')
-	echo "File -> $f -> $name"
-	if test -f "../../yata/media/analytics/api-v1_$name.json"; then
-            echo "api-v1_$name.json exists"
-	else
-	    echo "api-v1_$name.json create"
-            cat $f | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/api-v1_$name.json -p goaccess_public.conf --date-spec=hr --no-progress
-	fi
-	if test -f "../../yata/media/analytics/web_$name.json"; then
-            echo "web-v1_$name.json exists"
-	else
-	    echo "web-v1_$name.json create"
-            cat $f | sed '/static\|loot\/timings\|abroad\/export\|abroad\/import\|targets\/import\|targets\/export\|\/api\|\/share/d' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/web_$name.json -p goaccess_public.conf --date-spec=hr --no-progress
-	fi
-    done
+# check if needs to loop over last month/year directory
+if [[ "$m" == "$mp" ]] && [[ "$y" == "$yp" ]]; then
+    heads=("logs/$y/http-$y-$m-")
+else
+    heads=("logs/$y/http-$y-$m- logs/$yp/http-$yp-$mp-")
 fi
+
+# loop over the folders
+for head in $heads; do
+    echo "head" $head
+
+    # loop of the logs
+    for f in $head*; do
+	name=$(echo "$f" | sed -n 's/.*\([0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\).*/\1/p')
+	echo "file" $f
+
+	# check if api json file is missing to create the report
+	if [ ! -f ../../yata/media/analytics/api-v1_$name.json ]; then
+	    echo "api-v1_$name.json create"
+
+	    # check .log.gz or .log
+	    if [[ "$f" == *.gz ]]; then
+		zcat $f | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess - -o ../../yata/media/analytics/api-v1_$name.json -p ./goaccess_public.conf --date-spec=hr
+	    else
+		#cat $f | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/'
+		cat $f | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess - -o ../../yata/media/analytics/api-v1_$name.json -p ./goaccess_public.conf --date-spec=hr
+	    fi
+	fi
+
+	# check if web json file is missing to create the report
+	if [ ! -f ../../yata/media/analytics/web_$name.json ]; then
+	    echo "web_$name.json create"
+
+	    # check .log.gz or .log
+	    if [[ "$f" == *.gz ]]; then
+		zcat $f | sed '/static\|loot\/timings\|abroad\/export\|abroad\/import\|targets\/import\|targets\/export\|\/api\|\/share/d' | sed 's/?.*\sHTTP/ HTTP/' | goaccess - -o ../../yata/media/analytics/web_$name.json -p goaccess_public.conf --date-spec=hr
+	    else
+		cat $f | sed '/static\|loot\/timings\|abroad\/export\|abroad\/import\|targets\/import\|targets\/export\|\/api\|\/share/d' | sed 's/?.*\sHTTP/ HTTP/' | goaccess - -o ../../yata/media/analytics/web_$name.json -p goaccess_public.conf --date-spec=hr
+	    fi
+	fi
+
+    done
+done
--- setup/analytics/create_days.bash
+++ setup/analytics/create_days.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # old loot
-find ./logs -name "*.log" -print0 | xargs -0 cat | grep 'timings' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/old-loot_last-days.html -p goaccess_public.conf --date-spec=hr --no-progress
+#cat ./logs/*.log | grep 'timings' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/old-loot_last-days.html -p goaccess_public.conf --date-spec=hr
 
 # api v1
-find ./logs -name "*.log" -print0 | xargs -0 cat | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/api-v1_last-days.html -p goaccess_public.conf --date-spec=hr --no-progress
+cat ./logs/*/*.log | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess - -o ../../yata/media/analytics/api-v1_last-days.html -p goaccess_public.conf --date-spec=hr
 
+ls ./logs/*/*.log
 # web
-find ./logs -name "*.log" -print0 | xargs -0 cat | sed '/static\|loot\/timings\|abroad\/export\|abroad\/import\|targets\/import\|targets\/export\|\/api\|\/share/d' | sed 's/?.*\sHTTP/ HTTP/' | sed 's/bazaar\/update\/.*\sHTTP/bazaar\/update\/ HTTP/' | goaccess -o ../../yata/media/analytics/web_last-days.html -p goaccess_public.conf --date-spec=hr --no-progress
+cat ./logs/*/*.log | sed '/static\|loot\/timings\|abroad\/export\|abroad\/import\|targets\/import\|targets\/export\|\/api\|\/share/d' | sed 's/?.*\sHTTP/ HTTP/' | sed 's/bazaar\/update\/.*\sHTTP/bazaar\/update\/ HTTP/' | goaccess - -o ../../yata/media/analytics/web_last-days.html -p goaccess_public.conf --date-spec=hr
