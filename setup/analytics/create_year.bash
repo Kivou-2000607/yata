f81641c92612aaec7719435178d1441a91387ac8
Kivou [2000607] <kivou.2000607@gmail.com>
2020-11-10 16:35:18 +0100

    update analytics scripts

--- setup/analytics/create_year.bash
+++ setup/analytics/create_year.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "CREATE YEAR" $1
+
 # old loot
-# find ./logs/$1 -name http-$1-$2-*.log.gz -print0 | xargs -0 zcat | grep 'timings' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o reports/old-loot_$1-$2 -p goaccess_public.conf 
+zcat ./logs/$1/http-$1-*.log.gz | grep 'timings' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/old-loot_$1.html -p goaccess_public.conf 
 
 # api v1 
-find ./logs/$1 -name http-$1-*.log.gz -print0 | xargs -0 zcat | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/api-v1_$1.html -p goaccess_public.conf
+zcat ./logs/$1/http-$1-*.log.gz | grep '/api/v1/' | sed 's/?.*\sHTTP/ HTTP/' | goaccess -o ../../yata/media/analytics/api-v1_$1.html -p goaccess_public.conf
 
 # web
 zcat ./logs/$1/http-$1-*.log.gz | sed '/static\|loot\/timings\|abroad\/export\|abroad\/import\|targets\/import\|targets\/export\|\/api\|\/share/d' | sed 's/?.*\sHTTP/ HTTP/' | sed 's/bazaar\/update\/.*\sHTTP/bazaar\/update\/ HTTP/' | goaccess -o ../../yata/media/analytics/web_$1.html -p goaccess_public.conf
---
+++ setup/analytics/cron_weekly.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source source.bash
+
+t=$(date "+%Y" -d "4 day ago")
+t2=$(date "+%Y")
+
+echo $t
+./get.bash
+./create_year.bash $t
+if [ "$t2" != "$t" ]; then
+    echo $t2
+   ./create_year.bash $t2
+fi
+./push.bash
