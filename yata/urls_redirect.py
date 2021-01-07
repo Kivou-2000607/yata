38ab5ece1c1f16eab756fc50c42655f910cba3ba
Kivou-2000607 <kivou.2000607@gmail.com>
2021-01-07 23:01:50 +0100

    redirect

--- yata/urls_redirect.py
+++ yata/urls_redirect.py
@@ -5,14 +5,18 @@ from . import views_redirect
 from api.views import travel
 from api.views import targets
 from api.views import faction
+from api.views import loot
+from setup.views import analytics
 
 app_name = "yata"
 
 urlpatterns = [
-    re_path(r'^api/v1/travel/import/$', travel.importStocks, name="importStocks"),
-    re_path(r'^api/v1/travel/import/$', travel.importStocks, name="importStocks"),
-    re_path(r'^api/v1/targets/import/$', targets.importTargets, name="importTargets"),
-    re_path(r'^api/v1/faction/crimes/import/ranking/$', faction.updateRanking, name="updateRanking"),
-    re_path(r'^api/v1/faction/walls/import/$', faction.importWall, name="importWall"),
+    # re_path(r'^api/v1/loot/$', loot.loot),
+    re_path(r'^setup/analytics/$', analytics),
+    re_path(r'^loot/timings/$', loot.loot),
+    re_path(r'^api/v1/travel/import/$', travel.importStocks),
+    re_path(r'^api/v1/targets/import/$', targets.importTargets),
+    re_path(r'^api/v1/faction/crimes/import/ranking/$', faction.updateRanking),
+    re_path(r'^api/v1/faction/walls/import/$', faction.importWall),
     re_path(r'^.*$', views_redirect.index),
 ]
