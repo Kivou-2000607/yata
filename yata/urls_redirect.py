96320e2081be36eecd7288610a51dd50dbe9a6ef
Kivou-2000607 <kivou.2000607@gmail.com>
2021-01-09 00:03:22 +0100

    clean alwaysdata redirections

--- yata/urls_redirect.py
+++ yata/urls_redirect.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import include, re_path
 
 from . import views_redirect
 
@@ -6,19 +6,36 @@ from api.views import travel
 from api.views import targets
 from api.views import faction
 from api.views import loot
-#from setup.views import analytics
+from awards import views as views_awards
 
 app_name = "yata"
 
 urlpatterns = [
+
+    # for torn PDA
+    re_path(r'^awards/', include('awards.urls')),
+
+    # to support CORS
     re_path(r'^api/v1/loot/$', loot.loot),
     re_path(r'^api/v1/travel/export/$', travel.exportStocks),
 
-    re_path(r'^loot/timings/$', loot.loot),
-
+    # to close at the very end
     re_path(r'^api/v1/travel/import/$', travel.importStocks),
     re_path(r'^api/v1/targets/import/$', targets.importTargets),
     re_path(r'^api/v1/faction/crimes/import/ranking/$', faction.updateRanking),
     re_path(r'^api/v1/faction/walls/import/$', faction.importWall),
-    re_path(r'^.*$', views_redirect.index),
+
+    # hacks
+    re_path(r'', views_redirect.index, name="logout"),
+    re_path(r'', views_redirect.index, name="discord"),
+    re_path(r'', include('player.urls')),
+    re_path(r'', include('bazaar.urls')),
+    re_path(r'', include('company.urls')),
+    re_path(r'', include('stock.urls')),
+    re_path(r'', include('faction.urls')),
+    re_path(r'', include('target.urls')),
+    re_path(r'', include('loot.urls')),
+
+    # the redirection page
+    re_path(r'^.*$', views_redirect.index, name="index"),
 ]
