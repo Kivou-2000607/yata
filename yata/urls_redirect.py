abd1d41bb2e980a3f56154a72c631562d7db412d
Kivou-2000607 <kivou.2000607@gmail.com>
2021-01-08 16:54:27 +0100

    redirect

--- yata/urls_redirect.py
+++ yata/urls_redirect.py
@@ -6,14 +6,16 @@ from api.views import travel
 from api.views import targets
 from api.views import faction
 from api.views import loot
-from setup.views import analytics
+#from setup.views import analytics
 
 app_name = "yata"
 
 urlpatterns = [
-    # re_path(r'^api/v1/loot/$', loot.loot),
-    re_path(r'^setup/analytics/$', analytics),
+    re_path(r'^api/v1/loot/$', loot.loot),
+    re_path(r'^api/v1/travel/export/$', travel.exportStocks),
+
     re_path(r'^loot/timings/$', loot.loot),
+
     re_path(r'^api/v1/travel/import/$', travel.importStocks),
     re_path(r'^api/v1/targets/import/$', targets.importTargets),
     re_path(r'^api/v1/faction/crimes/import/ranking/$', faction.updateRanking),
--- yata/views_redirect.py
+++ yata/views_redirect.py
@@ -1,5 +1,9 @@
 import urllib
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+# def index(request):
+#     return HttpResponseRedirect(f'{urllib.parse.urljoin("https://yata.yt", request.path)}{f"?{urllib.parse.urlencode(request.GET)}" if len(request.GET) else ""}')
 
 def index(request):
-    return HttpResponseRedirect(f'{urllib.parse.urljoin("https://yata.yt", request.path)}{f"?{urllib.parse.urlencode(request.GET)}" if len(request.GET) else ""}')
+    return render(request, 'redirect.html')
