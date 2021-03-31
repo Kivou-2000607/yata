6f335689141c507a8b5939a1b60e666fc7c514ce
kivou-2000607 <kivou.2000607@gmail.com>
2021-03-31 17:15:03 +0200

    remove APP_DIR

--- yata/settings.py
+++ yata/settings.py
@@ -105,7 +105,6 @@ else:
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
             'DIRS': [os.path.join(BASE_DIR, 'templates')],
-            'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
                     'django.template.context_processors.debug',
