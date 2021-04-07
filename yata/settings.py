116fbd23d4db43f16bb4894c78bc9e99884bc4a9
kivou-2000607 <kivou.2000607@gmail.com>
2021-04-07 15:05:11 +0200

    send IP

--- yata/settings.py
+++ yata/settings.py
@@ -258,6 +258,7 @@ if config("ENABLE_SENTRY", default=False, cast=bool):
     sentry_sdk.init(
         dsn=config("SENTRY_DSN"),
         integrations=[DjangoIntegration()],
+        send_default_pii=True,
         traces_sample_rate=config("SENTRY_SAMPLE_RATE", default=0.0, cast=float),
         environment=config("SENTRY_ENVIRONMENT"),
     )
