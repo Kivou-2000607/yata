0f2c421f75cfcd1783d3a943aa2ddffcf57ca68c
kivou-2000607 <kivou.2000607@gmail.com>
2021-04-16 18:00:01 +0200

    Companies

--- yata/static/yata/js/company/company.js
+++ yata/static/yata/js/company/company.js
@@ -1,27 +1,9 @@
-$(document).ready(function () {
-    const h = $($(location).attr('hash'));
-    if (h.length) {
-        toggle_h(h);
-    }
-});
-
-// nav link
-$(document).on('click', 'table.company-categories td', function (e) {
-    e.preventDefault();
-    var l = $(this).children("a").attr("href").split("/")[2];
-    $("#content-update").load("/company/" + l + "/", {
-        csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
-    }, nav("/company/" + l + "/"));
-    $("#content-update h2").html(spinner + '&nbsp;&nbsp;Loading')
-    $("#content-update h2").addClass("grey");
-    $("div.error").hide();
-});
 
 // select a company
 $(document).on('change', '#company-select-form', e => {
     e.preventDefault();
     const reload = $("#company-details");
-    const company_id = $(e.currentTarget).children("p").children("select").val();
+    const company_id = $("#company-select-select").val();
     reload.load("/company/browse/", {
         type: "company-details",
         company_id: company_id,
@@ -58,7 +40,7 @@ $(document).on('change', '.company-employee-position-form', e => {
         employees_position_simu: JSON.stringify(employees_position),
         csrfmiddlewaretoken: getCookie("csrftoken")
     });
-    employee.html('<td colspan="11" style="text-align: center;">' + spinner + '</td>');
+    employee.html('<td colspan="13" style="text-align: center;">' + spinner + '</td>');
 });
 
 // apply suggestion
@@ -75,7 +57,7 @@ $(document).on('click', '#company-employees-simu-apply', e => {
         employees_position_simu: JSON.stringify(employees_position),
         csrfmiddlewaretoken: getCookie("csrftoken")
     });
-    employee.html('<td colspan="11" style="text-align: center;">' + spinner + '</td>');
+    employee.html('<td colspan="13" style="text-align: center;">' + spinner + '</td>');
 });
 
 // reset simu
