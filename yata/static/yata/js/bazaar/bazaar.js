05d596deea7a9fe828d84eee419297fd7fbd9e58
kivou-2000607 <kivou.2000607@gmail.com>
2021-04-23 00:03:43 +0200

    rebuild item tables

--- yata/static/yata/js/bazaar/bazaar.js
+++ yata/static/yata/js/bazaar/bazaar.js
@@ -1,18 +1,4 @@
 // show/hide details item
-$(document).on('click', '.details-item', function(e){
-    e.preventDefault();
-    var tId = $(this).attr("data-val");
-    $( "#bazaar-modal" ).load( "/bazaar/details/"+tId, {
-        csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
-    });
-});
-$(document).on('click', '.prices-item', function(e){
-    e.preventDefault();
-    var tId = $(this).attr("data-val");
-    $( "#bazaar-modal" ).load( "/bazaar/prices/"+tId, {
-        csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
-    });
-});
 $(document).on('click', 'tr.abroad-item-stocks > td:not(.dont-touch-me)', function(e){
     e.preventDefault();
     var item_id = $(this).parent("tr").attr("data-ite");
@@ -22,120 +8,68 @@ $(document).on('click', 'tr.abroad-item-stocks > td:not(.dont-touch-me)', functi
     });
 });
 
-// $(document).on('click', '.close', function(e){
-//     e.preventDefault();
-//     $(this).parent("div.container").css("display", "none");
-// });
 
-// update item
-$(document).on('click', '.update-item', function(e){
+// header click
+$(document).on('click', '.item-table-header:not(.no-click)', e=>{
     e.preventDefault();
-    if ($(this).hasClass("lock")) {
-        console.log("cooldown");
-    } else {
-        var tId = $(this).attr("href").split("/").pop();
-        $( "#item-sell-table-" + tId ).load( "/bazaar/update/"+tId, {
-            csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
-        });
-        $(this).children("button").html('<i class="fas fa-spinner fa-pulse"></i>');
+
+    // get item data and DOM
+    const action = $(e.currentTarget).attr('data-act');
+    const item_table = $(e.currentTarget).parents('.item-table');
+    const item_id = $(item_table).attr('data-iid');
+    console.log(action, item_id);
+
+    if( action == "update" ) {
+      $(item_table).load( "/bazaar/update/"+item_id, { csrfmiddlewaretoken: getCookie("csrftoken"), });
+      $(e.currentTarget).find("button").html('<i class="fas fa-spinner fa-pulse"></i>');
+    } else if (action == "delete") {
+      $(item_table).load( "/bazaar/delete/"+item_id, { csrfmiddlewaretoken: getCookie("csrftoken"), });
+      $(e.currentTarget).html('<i class="fas fa-spinner fa-pulse"></i>');
+    } else if (action == "details") {
+      $( "#bazaar-modal" ).load( "/bazaar/details/"+item_id, { csrfmiddlewaretoken: getCookie("csrftoken"), });
+    } else if (action == "toggle") {
+      $(item_table).load( "/bazaar/toggle/"+item_id, { csrfmiddlewaretoken: getCookie("csrftoken"), });
+    } else if (action == "prices") {
+      $( "#bazaar-modal" ).load( "/bazaar/prices/"+item_id, { csrfmiddlewaretoken: getCookie("csrftoken"), });
     }
 });
 
 // update type
-$(document).on('click', '.update-type', function(e){
+$(document).on('click', 'span.update-type', e=>{
     e.preventDefault();
-    console.log("coucou")
-    var tType = $(this).attr("href").split("/").pop();
-    var i = 1;
-    $("#loop-over-item-sell-table-"+tType).find('table[id^="item-sell-table-"]').each(function() {
-        var reload = $(this);
-        var tId = $(this).attr("id").split("-").pop();
-        var wait = i*500 + parseInt(i/10)*3000;
+    const type = $(e.currentTarget).attr("data-val");
+    let i = 1;
+    console.log(type)
+    $("#loop-over-item-sell-table-"+type).find('div.item-table').each(function() {
+        let item_table = $(this)
+        let item_id = $(this).attr("data-iid");
+        let wait = i*500 + parseInt(i/10)*3000;
+        // console.log(item_id, $(this));
         (function(index) {
             setTimeout(function() {
-                reload.load( "/bazaar/update/"+tId, {
-                    csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
-                });
-                reload.find("button").html('<i class="fas fa-spinner fa-pulse"></i>');
+                $(item_table).load( "/bazaar/update/"+item_id, { csrfmiddlewaretoken:  getCookie("csrftoken"), });
+                $(item_table).find("button").html('<i class="fas fa-spinner fa-pulse"></i>');
+                console.log($(item_table).find("button"));
              }, wait);
         })(i);
         i++;
     });
 });
 
-// delete item
-$(document).on('click', '.delete-item', function(e){
-    e.preventDefault();
-    var tId = $(this).attr("href").split("/").pop();
-    $( "#item-sell-table-" + tId ).load( "/bazaar/delete/"+tId, {
-        csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
-    });
-    $(this).closest("thead").children("tr:first-child").children("th").children("div").children("form").children("a").children("button").html('<i class="fas fa-spinner fa-pulse"></i>');
-});
-
-// toggle item
-$(document).on('click', '.toggle-item', function(e){
-    e.preventDefault();
-    var tId = $(this).attr("href").split("/").pop();
-    $( "#item-sell-table-" + tId ).load( "/bazaar/toggle/"+tId, {
-        csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
-    });
-    $(this).closest("thead").children("tr:first-child").children("th").children("div").children("form").children("a").children("button").html('<i class="fas fa-spinner fa-pulse"></i>');
-});
-
 // refresh timer update
 window.setInterval(function(){
-    $("a.update-timer").each(function() {
-        table = $(this).closest("table");
-        var updatetime = $.trim($(this).text());
-        if ( updatetime.search(" s")!=-1 ) {
-            var splt = updatetime.split(" ");
-            var seconds = 0;
-            if (splt.length == 2) {
-                seconds = parseInt(splt[0]);
-            } else if (splt.length == 4) {
-                seconds = parseInt(splt[2]) + 60 * parseInt(splt[0]);
-            }
+    $(".update-timer").each(function() {
+        var tsUpdate = parseInt($(this).attr("data-val"));
+        var tsNow = parseInt(Date.now() / 1000)
 
-            // if ( seconds > 10 ) {
-            //     console.log(table.find(".lock"));
-            // }
-
-            if ( seconds > 599 ) {
-                $(this).html("> 10 mins");
-                // table.addClass("old-refresh");
-            } else {
-                seconds ++;
-                minutes = Math.floor(seconds / 60);
-                seconds = seconds % 60;
-                if (minutes) {
-                    spad = ("0"+seconds.toString()).slice(-2);
-                    $(this).html(minutes.toString()+" mins "+spad+" s");
-                } else {
-                    $(this).html(seconds.toString()+" s");
-                }
-                // table.removeClass("old-refresh");
-            }
+        if ( tsNow - tsUpdate > 300 ) {
+            $(this).html("> 5 min");
+            $(this).removeClass("update-timer")
+        } else {
+            $(this).html(fancyTimeFormat(tsNow - tsUpdate))
         }
-        // else {
-        //     if (!table.hasClass('old-refresh')) {
-        //         table.addClass('old-refresh');
-        //     }
-        // }
     });
 }, 1000);
-
-
-// show/hide module
-$(document).on('click', 'h3.toggle-display', function(e){
-    e.preventDefault();
-    var h = $(this);
-    var c = h.next("pre");
-    var i = h.find("i");
-    if (c.is(":hidden")) i.addClass("fa-rotate-90"); else i.removeClass("fa-rotate-90");
-    c.slideToggle("fast");
-});
-
 
 
 // toggle abroad filters
