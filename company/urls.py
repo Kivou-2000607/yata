d724863868f3c249d79d9503a987fbb04d9d1c67
Kivou-2000607 <kivou.2000607@gmail.com>
2022-04-14 09:59:27 +0200

    company links

--- company/urls.py
+++ company/urls.py
@@ -8,8 +8,8 @@ app_name = "company"
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     re_path(r'^supervise/$', views.supervise, name='supervise'),
-    re_path(r'^supervise/manage$', views.manageCompany, name='manage'),
+    re_path(r'^supervise/manage/$', views.manageCompany, name='manage'),
     re_path(r'^browse/$', views.browse, name='browse'),
-    re_path(r'^browse/companies$', views.companies, name='companies'),
+    re_path(r'^browse/companies/$', views.companies, name='companies'),
     # re_path(r'^ws/$', views.ws, name='ws'),
 ]
