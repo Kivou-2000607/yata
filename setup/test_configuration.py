42142195fc7bb503d7e512e8c503a95c1a62f36e
Morgan <morgan35543@gmail.com>
2022-04-25 19:34:31 +0100

    Updated unit test for configuration

--- setup/test_configuration.py
+++ setup/test_configuration.py
@@ -6,10 +6,7 @@ from setup.configuration import Configuration
 
 
 class TestConfiguration(unittest.TestCase):
-    def setUp(self):
-        self.recursion_counter = 0
-
-    def testInput(self, input_tuple):
+    def validInput(self, input_tuple):
         config = Configuration()
         config.getInput = MagicMock(return_value=input_tuple[0])
 
@@ -18,14 +15,23 @@ class TestConfiguration(unittest.TestCase):
         config.getInput.assert_called_with("test (y/n): ")
         self.assertEqual(result, input_tuple[1])
 
+    def invalidInput(self, input_tuple, mock_call_count):
+        config = Configuration()
+        config.getInput = MagicMock(side_effect=input_tuple[0])
+
+        result = config.question("test")
+
+        self.assertEqual(config.getInput.call_count, mock_call_count)
+        self.assertEqual(result, input_tuple[1])
+
     def test_valid_inputs(self):
         inputs = [['yes', True], ['ye', True], ['y', True], ['no', False], ['n', False]]
 
         for input_tuple in inputs:
-            self.testInput(input_tuple)
+            self.validInput(input_tuple)
 
-    def test_invalid_inputs(self):
-        inputs = [['invalid', False], ['a', False]]
+    def test_invalid_inputs(self): 
+        inputs = [[['invalid', 'maybe', 'uncertain', 'no'], False], [['a', 'yes'], True]]
 
         for input_tuple in inputs:
-            self.testInput(input_tuple)
+            self.invalidInput(input_tuple, len(input_tuple[0]))
