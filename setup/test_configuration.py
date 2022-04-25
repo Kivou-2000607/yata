4bc67d8cc1cfa6f446c20bab2c9540a0bab5fcf5
Morgan <morgan35543@gmail.com>
2022-04-25 14:24:31 +0100

    -

--- setup/test_configuration.py
+++ setup/test_configuration.py
@@ -23,9 +23,3 @@ class TestConfiguration(unittest.TestCase):
 
         for input_tuple in inputs:
             self.testInput(input_tuple)
-
-    def test_invalid_inputs(self):
-        inputs = [['invalid', False], ['a', False]]
-
-        for input_tuple in inputs:
-            self.testInput(input_tuple)
