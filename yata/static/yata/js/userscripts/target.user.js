1e5a2afc7dd0305b52f6fbace5d9ce8da0c13204
SVD-NL <72652479+SVD-NL@users.noreply.github.com>
2022-05-20 10:51:12 +0200

    Add error handling, add option toggle

--- yata/static/yata/js/userscripts/target.user.js
+++ yata/static/yata/js/userscripts/target.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Yata Target Import
-// @version      0.1
+// @version      0.2
 // @description  Import target to yata from a profile page
-// @updateURL    about:blank
-// @author       SVD_NL
+// @downloadURL  https://github.com/Kivou-2000607/yata/tree/99-script-to-importexport-target-from-profile/yata/static/yata/js/userscripts/target.user.js
+// @updateURL    https://github.com/Kivou-2000607/yata/tree/99-script-to-importexport-target-from-profile/yata/static/yata/js/userscripts/target.user.js
+// @author       SVD_NL [2363978]
 // @run-at       document-end
 // @match        https://www.torn.com/profiles.php?XID=*
 // @grant        GM_xmlhttpRequest
@@ -34,30 +35,34 @@
         $("#profileroot").before(infobox);
         let addButton = $("<button class='YATA-button'>")
             .text("Add to target list")
-            .click( () => sendTarget());
-        let targetDetailsButton = $("<button class='YATA-button'>")
-            .text("Target details")
             .click( () => addOptionBox());
         let apiButton = $("<button class='YATA-button'>")
             .text("API Key")
             .click( () => errorKey("",true));
         $("#YATA-message-buttons")
             .append(addButton)
-            .append(targetDetailsButton)
             .append(apiButton);
     }
 
     function addOptionBox() {
-        let optionBox = $("<div id=YAYA-target-option-box class='YATA-messagebox'></div>");
+        if ($("#YATA-target-option-box").length) {
+            $("#YATA-target-option-box").toggle();
+            return;
+        }
+        let optionBox = $("<div id=YATA-target-option-box class='yata-messagebox'></div>");
         let noteInput = $("<input id='YATA-target-note' class='YATA-input' placeholder='Enter note'>");
         let colorPicker = $("<select id='YATA-target-color' class='YATA-select'></select>")
             .append("<option value=0>Black</option>")
             .append("<option value=1>Green</option>")
             .append("<option value=2>Yellow</option>")
             .append("<option value=3>Red</option>");
+        let importButton = $("<button class='YATA-button'>")
+            .text("Add Target")
+            .click( () => sendTarget());
         optionBox
             .append(noteInput)
-            .append(colorPicker);
+            .append(colorPicker)
+            .append(importButton);
         
         $("#YATA-infobox").append(optionBox);
     }
@@ -68,8 +73,32 @@
             method: "POST",
             url: yataTargetUrl,
             data: JSON.stringify(payload),
-            onload: response => {alert(response.responseText)}
-    });
+            onload: resp => {
+                try {
+                    const obj = JSON.parse(resp.responseText);
+                    console.log(resp.status);
+                    console.log(obj);
+                    if (resp.status === 400) {
+                        if (obj.error.code === 4) error(`API error (${obj.error.error})`);
+                        else error(`User error (${obj.error.error})`);
+                    }
+                    else if (resp.status === 500) error("Server error (" + obj.error.error + ")");
+                    else if (resp.status === 200) valid(obj.message);
+                    else error("No message received from the server");
+                } catch (e) {
+                    error("Failed to parse response from server" + e);
+                }
+            },
+            ontimeout: () => {
+                error("Request has timed out");
+            },
+            onerror: () => {
+                error("Unknown error has occured when trying to send the data");
+            },
+            onabort: () => {
+                error("Upon sending the data, the request was canceled");
+            }
+        });
     }
 
     function constructJSON() {
