1bd3ded0421bbdc1228d2ab0a29699b97afecf20
Kivou-2000607 <kivou.2000607@gmail.com>
2023-10-27 23:12:45 +0200

    fix bse

--- api/views/bse.py
+++ api/views/bse.py
@@ -35,9 +35,9 @@ from yata.handy import apiCall
 
 try:
     print("Load models")
-    load_lvl1= ["10k_to_20b"]
-    load_lvl2= ["10k_to_100m", "100m_to_20b", "90m_to_110m"]
-    load_lvl3= ["10k_to_1m", "1m_to_100m", "100m_to_1b", "1b_to_20b"]
+    load_lvl1 = ["10k_to_20b"]
+    load_lvl2 = ["10k_to_100m", "100m_to_20b", "90m_to_110m"]
+    load_lvl3 = ["10k_to_1m", "1m_to_100m", "100m_to_1b", "1b_to_20b"]
 
     MODELS = []
     MODELS.append([cloudpickle.load(open(f"nn/model_{name}.pk", "rb")) for name in load_lvl1])
@@ -45,8 +45,17 @@ try:
     MODELS.append([cloudpickle.load(open(f"nn/model_{name}.pk", "rb")) for name in load_lvl3])
 
     # MODEL = cloudpickle.load(open("model.pk", "rb"))
-except Exception as e:
-    MODEL = str(e)
+except Exception:
+    MODELS = []
+
+
+def human_format(num):
+    num = float(f"{num:.3g}")
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return f"{num:f}".rstrip("0").rstrip(".") + ["", "k", "m", "b", "t"][magnitude]
 
 
 def predict(models, d):
@@ -54,8 +63,8 @@ def predict(models, d):
         model = m["model"]
         pipeline = m["pipeline"]
         labels = m["labels"]
-        model_min = m["meta"]["stats"][0]
-        model_max = m["meta"]["stats"][1]
+        model_min = m["meta"]["stats"][0] if "meta" in m else m["min"]
+        model_max = m["meta"]["stats"][1] if "meta" in m else m["max"]
 
         X = numpy.zeros((1, len(labels)))
         for icol, stat in enumerate(labels):  # loop over stats
@@ -70,8 +79,7 @@ def predict(models, d):
     lvl = 0
     prediction, model_min, model_max = predict_from_single_model(models[lvl][0], d)
     bs = prediction[1]
-    # print(f"LVL1__: {human_format(model_min):>6} {human_format(model_max):>6} / {human_format(bs):>6} ({human_format(truth):>6})")
-
+    # print(f"LVL1__: {human_format(model_min):>6} {human_format(model_max):>6} / {human_format(bs):>6}")
 
     # level 1
     lvl = 1
@@ -80,7 +88,7 @@ def predict(models, d):
         i = 2
     prediction, model_min, model_max = predict_from_single_model(models[lvl][i], d)
     bs = prediction[1]
-    # print(f"LVL2_{i}: {human_format(model_min):>6} {human_format(model_max):>6} / {human_format(bs):>6} ({human_format(truth):>6})")
+    # print(f"LVL2_{i}: {human_format(model_min):>6} {human_format(model_max):>6} / {human_format(bs):>6}")
 
     # level 2
     lvl = 2
@@ -90,18 +98,18 @@ def predict(models, d):
     bs = prediction[1]
     score = prediction[0]
     skewness = prediction[2]
-    # print(f"LVL3_{2 * i + j}: {human_format(model_min):>6} {human_format(model_max):>6} / {human_format(bs):>6} ({human_format(truth):>6})")
+    # print(f"LVL3_{2 * i + j}: {human_format(model_min):>6} {human_format(model_max):>6} / {human_format(bs):>6}")
 
-    print(bs, score, skewness)
+    # print(bs, score, skewness)
     return bs, score, skewness
 
 
 # @cache_page(24 * 3600)
 def bs(request, target_id):
     try:
-
         # get cache
         response = cache.get(f"nn-stats-{target_id}", False)
+        # response = False
         if response:
             print(f"[bs] {target_id} (cache)")
             return JsonResponse(response, status=200)
@@ -112,20 +120,29 @@ def bs(request, target_id):
             return JsonResponse({"error": {"code": 2, "error": "No keys provided"}}, status=400)
 
         # test model
-        if isinstance(MODELS, str):
-            return JsonResponse({"error": {"code": 1, "error": MODELS}}, status=500)
+        if not len(MODELS):
+            return JsonResponse({"error": {"code": 1, "error": "no models found"}}, status=500)
 
         selections = ["profile", "personalstats", "discord", "timestamp"]
         r = apiCall("user", target_id, ",".join(selections), key=key)
         if "apiError" in r:
             return JsonResponse({"error": {"code": 4, "error": r["apiErrorString"]}}, status=400)
 
-        bs, score, skewness = predict(MODELS, r)
+        bs, score, skewness = predict(MODELS, r["personalstats"])
         build_type = "Defensive" if skewness < 0 else "Offensive"
         skewness = abs(int(100 * skewness))
         build_type = "Balanced" if not bs else build_type
 
-        response = {r["player_id"]: {"total": int(bs), "score": int(score), "type": build_type, "skewness": int(skewness), "timestamp": int(time.time()), "version": 0.1}}
+        response = {
+            r["player_id"]: {
+                "total": int(bs),
+                "score": int(score),
+                "type": build_type,
+                "skewness": int(skewness),
+                "timestamp": int(time.time()),
+                "version": 1,
+            }
+        }
         cache.set(f"nn-stats-{target_id}", response, 3600 * 24)
 
         return JsonResponse(response, status=200)
