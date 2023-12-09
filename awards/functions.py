0cd8a86421ae52f8c30d97f9c9ef8ce3e72600cb
Kivou-2000607 <kivou.2000607@gmail.com>
2023-12-09 22:44:50 +0100

    fix war machine rounding

--- awards/functions.py
+++ awards/functions.py
@@ -592,7 +592,8 @@ def createAwards(tornAwards, userInfo, category, pinned=False):
                     bridge = sorted(bridge.values(), key=lambda x: -x[1])
                     vp["current"] = current
                     vp["achieve"] = min(1, float(vp["current"]) / float(vp["goal"]))
-                    vp["comment"] = "<br>".join(["<b class={}>{}</b>: {:,d} ({:.0f}%)".format(v[2], v[0], v[1], 100 * min(1, v[1] / nHits)) for v in bridge])
+                    p = int(100 * min(1, v[1] / nHits))
+                    vp["comment"] = "<br>".join([f"<b class={v[2]}>{v[0]}</b>: {v[1]:,d} ({p}%)" for v in bridge])
                     awards[type]["h_" + k] = vp
 
                 elif int(k) in [828, 871]:
