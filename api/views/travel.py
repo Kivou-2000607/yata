2a747b6213d970613b3356014a23dfe55d86a7b6
Decap [47662] <you@example.com>
2024-07-23 13:02:15 +0100

    Added card skimmer and glowstick

--- api/views/travel.py
+++ api/views/travel.py
@@ -16,49 +16,47 @@ This file is part of yata.
     You should have received a copy of the GNU General Public License
     along with yata. If not, see <https://www.gnu.org/licenses/>.
 """
-# django
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.core.cache import cache
-
-# cache and rate limit
-from django.utils.decorators import method_decorator
-from yata.decorators import never_ever_cache
-
 # standards
 import json
 
+from django.core.cache import cache
+
+# django
+from django.http import JsonResponse
+
+# cache and rate limit
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+from bazaar.countries import countries
+from bazaar.models import AbroadStocks, Item, VerifiedClient
+from yata.decorators import never_ever_cache
+
 # yata
 from yata.handy import tsnow
-from yata.handy import clear_cf_cache
-from bazaar.countries import countries
-from bazaar.models import AbroadStocks
-from bazaar.models import VerifiedClient
-from bazaar.models import Item
 
 items_table = {
-  "mex" : [159, 432, 426, 260, 230, 99, 259, 110, 8, 175, 178, 111, 258, 26, 229, 409, 20, 231, 232, 177, 11, 327, 640, 107, 31, 50, 108, 21, 63, 399],
-  "cay" : [618, 620, 619, 617, 626, 623, 624, 616, 622, 621, 625, 613, 612, 614, 615],
-  "can" : [261, 253, 262, 263, 252, 402, 410, 413, 254, 645, 328, 196, 197, 201, 205, 206, 1348],
-  "haw" : [243, 264, 265, 241, 242, 430, 419, 420, 421, 240],
-  "uni" : [266, 439, 431, 218, 268, 416, 267, 221, 418, 220, 438, 411, 408, 415, 641, 217, 219, 397, 196, 197, 198, 201, 203, 205, 206, 1246],
-  "arg" : [270, 269, 271, 407, 256, 257, 391, 255, 333, 196, 198, 199, 203, 204],
-  "swi" : [273, 272, 224, 436, 222, 223, 361, 398, 435, 196, 198, 199, 201, 203, 204],
-  "jap" : [429, 294, 427, 277, 433, 239, 238, 234, 434, 235, 278, 236, 279, 233, 237, 395, 334, 197, 198, 200, 203, 204, 205, 206],
-  "chi" : [274, 246, 245, 244, 276, 249, 275, 247, 250, 335, 251, 326, 248, 400, 197, 199, 200, 201, 204],
-  "uae" : [385, 412, 384, 414, 440, 381, 383, 386, 387, 388, 382],
-  "sou" : [281, 280, 227, 4, 282, 228, 226, 406, 225, 654, 653, 652, 651, 332, 199, 200, 201, 203, 206],
+    "mex": [159, 432, 426, 260, 230, 99, 259, 110, 8, 175, 178, 111, 258, 26, 229, 409, 20, 231, 232, 177, 11, 327, 640, 107, 31, 50, 108, 21, 63, 399, 1125],
+    "cay": [618, 620, 619, 617, 626, 623, 624, 616, 622, 621, 625, 613, 612, 614, 615],
+    "can": [261, 253, 262, 263, 252, 402, 410, 413, 254, 645, 328, 196, 197, 201, 205, 206, 1348],
+    "haw": [243, 264, 265, 241, 242, 430, 419, 420, 421, 240],
+    "uni": [266, 439, 431, 218, 268, 416, 267, 221, 418, 220, 438, 411, 408, 415, 641, 217, 219, 397, 196, 197, 198, 201, 203, 205, 206, 1246],
+    "arg": [270, 269, 271, 407, 256, 257, 391, 255, 333, 196, 198, 199, 203, 204],
+    "swi": [273, 272, 224, 436, 222, 223, 361, 398, 435, 196, 198, 199, 201, 203, 204],
+    "jap": [429, 294, 427, 277, 433, 239, 238, 234, 434, 235, 278, 236, 279, 233, 237, 395, 334, 197, 198, 200, 203, 204, 205, 206, 437],
+    "chi": [274, 246, 245, 244, 276, 249, 275, 247, 250, 335, 251, 326, 248, 400, 197, 199, 200, 201, 204],
+    "uae": [385, 412, 384, 414, 440, 381, 383, 386, 387, 388, 382],
+    "sou": [281, 280, 227, 4, 282, 228, 226, 406, 225, 654, 653, 652, 651, 332, 199, 200, 201, 203, 206],
 }
-
 
 
 @method_decorator(never_ever_cache)
 def exportStocks(request):
     try:
 
-        c = cache.get(f"foreign_stocks_payload", False)
+        c = cache.get("foreign_stocks_payload", False)
         if c:
-            #print("[api.travel.export] get stocks (cache)")
+            # print("[api.travel.export] get stocks (cache)")
             return JsonResponse(c, status=200)
 
         print("[api.travel.export] get stocks (db) start")
@@ -67,10 +65,19 @@ def exportStocks(request):
         #     return JsonResponse({"error": {"code": 3, "error": "Too many requests (10 calls / hour)"}}, status=429)
 
         stocks = AbroadStocks.objects.filter(last=True)
-        countries = {"mex": {}, "cay": {}, "can": {},
-                     "haw": {}, "uni": {}, "arg": {},
-                     "swi": {}, "jap": {}, "chi": {},
-                     "uae": {}, "sou": {},}
+        countries = {
+            "mex": {},
+            "cay": {},
+            "can": {},
+            "haw": {},
+            "uni": {},
+            "arg": {},
+            "swi": {},
+            "jap": {},
+            "chi": {},
+            "uae": {},
+            "sou": {},
+        }
 
         for k in countries:
             c_stocks = stocks.filter(country_key=k)
@@ -79,13 +86,10 @@ def exportStocks(request):
             else:
                 countries[k] = {"update": 0, "stocks": []}
 
-        payload = {
-            "stocks": countries,
-            "timestamp": tsnow()
-        }
+        payload = {"stocks": countries, "timestamp": tsnow()}
 
-        cache.set(f"foreign_stocks_payload", payload, 3600)
-        #print("[api.travel.export] get stocks (db) cache set")
+        cache.set("foreign_stocks_payload", payload, 3600)
+        # print("[api.travel.export] get stocks (db) cache set")
         return JsonResponse(payload, status=200)
 
     except BaseException as e:
@@ -95,18 +99,18 @@ def exportStocks(request):
 @csrf_exempt
 def importStocks(request):
     try:
-        if request.method != 'POST':
+        if request.method != "POST":
             return JsonResponse({"error": {"code": 2, "error": "POST request needed"}}, status=400)
 
         try:
             payload = json.loads(request.body)
-        except BaseException as e:
+        except BaseException:
             return JsonResponse({"error": {"code": 2, "error": "wrong body format"}}, status=400)
 
         # check mandatory keys
         for key in ["country", "items"]:
             if key not in payload:
-                return JsonResponse({"error": {"code": 2, "error": f'Missing \'{key}\' key in payload"'}}, status=400)
+                return JsonResponse({"error": {"code": 2, "error": f"Missing '{key}' key in payload\""}}, status=400)
 
         # check items length
         if not len(payload["items"]):
@@ -115,11 +119,11 @@ def importStocks(request):
         # check country:
         country_key = str(payload["country"]).lower().strip()[:3]
         if country_key not in countries:
-            return JsonResponse({"error": {"code": 2, "error": f'Unknown country key \'{country_key}\''}}, status=400)
+            return JsonResponse({"error": {"code": 2, "error": f"Unknown country key '{country_key}'"}}, status=400)
 
         if cache.get(f"foreign_stocks_lock_{country_key}", False):
             print("[api.travel.import] ignore update", country_key)
-            return JsonResponse({"message": f"The stocks have been updated less than 60s ago"}, status=200)
+            return JsonResponse({"message": "The stocks have been updated less than 60s ago"}, status=200)
         else:
             cache.set(f"foreign_stocks_lock_{country_key}", True, 60)
             print("[api.travel.import] update stocks", country_key)
@@ -139,7 +143,7 @@ def importStocks(request):
             for item in items_list:
                 for key in ["id", "quantity", "cost"]:
                     if not str(item.get(key)).isdigit():
-                        return JsonResponse({"error": {"code": 2, "error": f'Wrong {key} for item object: {item.get(key)}'}}, status=400)
+                        return JsonResponse({"error": {"code": 2, "error": f"Wrong {key} for item object: {item.get(key)}"}}, status=400)
 
                 items[int(item["id"])] = {"cost": int(item["cost"]), "quantity": int(item["quantity"])}
 
@@ -148,11 +152,11 @@ def importStocks(request):
             cast_to_int = []
             for item_id, item in items.items():
                 if not str(item_id).isdigit():
-                    return JsonResponse({"error": {"code": 2, "error": f'Wrong item id {item_id}'}}, status=400)
+                    return JsonResponse({"error": {"code": 2, "error": f"Wrong item id {item_id}"}}, status=400)
 
                 for key in ["quantity", "cost"]:
                     if not str(item.get(key)).isdigit():
-                        return JsonResponse({"error": {"code": 2, "error": f'Wrong item {key} for item {item_id}'}}, status=400)
+                        return JsonResponse({"error": {"code": 2, "error": f"Wrong item {key} for item {item_id}"}}, status=400)
 
                 item = {"cost": int(item["cost"]), "quantity": int(item["quantity"])}
                 if not isinstance(item_id, int):
@@ -167,7 +171,7 @@ def importStocks(request):
             return JsonResponse({"error": {"code": 2, "error": "Expecting a POST request"}}, status=400)
 
         # get all unique items from this country
-        distinct_items = [k['item_id'] for k in AbroadStocks.objects.filter(country_key=country_key).values('item_id').distinct()]
+        distinct_items = [k["item_id"] for k in AbroadStocks.objects.filter(country_key=country_key).values("item_id").distinct()]
 
         # add items not in db
         # for all keys and values to be int
@@ -193,13 +197,7 @@ def importStocks(request):
                 cost = 0 if lastItem is None else lastItem.cost
                 quantity = 0
 
-            stocks[item_id] = {"country": country,
-                               "country_key": country_key,
-                               "client": "{} [{}]".format(client_name, client_version),
-                               "timestamp": timestamp,
-                               "cost": cost,
-                               "quantity": quantity
-                               }
+            stocks[item_id] = {"country": country, "country_key": country_key, "client": "{} [{}]".format(client_name, client_version), "timestamp": timestamp, "cost": cost, "quantity": quantity}
 
         client, _ = VerifiedClient.objects.get_or_create(name=client_name, version=client_version)
         client.update_author(payload)
@@ -216,12 +214,12 @@ def importStocks(request):
             # clear cloudflare cache
             # r = clear_cf_cache(["https://yata.yt/api/v1/travel/export/*"])
             # print("[api.travel.import] clear cloudflare cache", r)
-            #print("[api.travel.import] clear droplet cache")
-            cache.delete(f"foreign_stocks_payload")
+            # print("[api.travel.import] clear droplet cache")
+            cache.delete("foreign_stocks_payload")
             return JsonResponse({"message": f"The stocks have been updated with {client}"}, status=200)
 
         else:
-            msg = f'Your client \'{client_name} [{client_name}]\' made a successful request but has not been added to the official API list. If you feel confident it\'s working correctly contact Kivou [2000607] to start updating the database.'
+            msg = f"Your client '{client_name} [{client_name}]' made a successful request but has not been added to the official API list. If you feel confident it's working correctly contact Kivou [2000607] to start updating the database."
             return JsonResponse({"message": msg, "stocks": stocks}, status=200)
 
     except BaseException as e:
