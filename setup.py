9a0cd68e4a29e39be1fb3f3ca75acfcbb6400dca
Decap <Decap>
2025-01-31 13:09:26 +0000

    Updated with new logger

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import django_json_widget as _
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yata.settings")
 django.setup()
-
+from yata.logger import logger
 from decouple import config  # noqa: E402
 from django.contrib.auth.models import User  # noqa: E402
 
@@ -41,12 +41,12 @@ static_file = yes_or_no("Do you want to generate static files?")
 
 if reset_db:
     if config("DATABASE") == "postgresql":
-        print("Remove local database")
+        logger.info("Remove local database")
         cmd = "python manage.py reset_db"
         r = os.system(cmd)
     else:
         # remove local database
-        print("Remove local database")
+        logger.info("Remove local database")
         cmd = "rm -vf db.sqlite3"
         r = os.system(cmd)
 
@@ -60,49 +60,49 @@ if reset_db:
 
 # create db super user
 if not len(User.objects.all()):
-    print("create superuser")
+    logger.info("create superuser")
     User.objects.create_superuser("admin", "admin@example.com", "adminpass")
 
 # create required objects
 if not len(PlayerData.objects.all()):
-    print("Create Players stats")
+    logger.info("Create Players stats")
     cmd = "python manage.py players_stats"
     r = os.system(cmd)
 
 if not len(Player.objects.filter(tId=-1)):
-    print("Create Player")
+    logger.info("Create Player")
     Player.objects.create(tId=-1, name="Anonymous", validKey=False)
 
 if not len(Faction.objects.filter(tId=-1)):
-    print("Create Faction")
+    logger.info("Create Faction")
     Faction.objects.create(tId=-1, name="Faction Anonymous")
 
 if not len(APIKey.objects.all()) and key:
-    print("Create API Key")
+    logger.info("Create API Key")
     APIKey.objects.create(key=key)
 
 if not len(AwardsData.objects.all()):
-    print("Create Awards data")
+    logger.info("Create Awards data")
     AwardsData.objects.create()
 
 if not len(BazaarData.objects.all()):
-    print("Create Bazaar data")
+    logger.info("Create Bazaar data")
     BazaarData.objects.create()
 
 if not len(FactionData.objects.all()):
-    print("Create Faction data")
+    logger.info("Create Faction data")
     FactionData.objects.create()
     cmd = "python manage.py init_faction_tree"
     r = os.system(cmd)
 
 if not len(NPC.objects.all()):
-    print("Create NPC")
+    logger.info("Create NPC")
     NPC.objects.create(tId=4, show=True)
     NPC.objects.create(tId=15, show=True)
     NPC.objects.create(tId=19, show=True)
 
 if not len(CompanyDescription.objects.all()):
-    print("Create NPC")
+    logger.info("Create NPC")
     cmd = "python manage.py init_companies"
     r = os.system(cmd)
 
