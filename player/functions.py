96b590e14458d312e1050f338420e5d17c1a1992
Decap <Decap>
2025-10-10 10:56:57 +0100

    stash

--- player/functions.py
+++ player/functions.py
@@ -1,44 +1,39 @@
 # Copyright 2019 kivou.2000607@gmail.com
-# 
+#
 # This file is part of yata.
-# 
+#
 #     yata is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
 #     any later version.
-# 
+#
 #     yata is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
-# 
+#
 #     You should have received a copy of the GNU General Public License
 #     along with yata. If not, see <https://www.gnu.org/licenses/>.
 
-from django.db import models
+
 from django.utils import timezone
 
-import json
-
-from yata.handy import *
-from awards.functions import updatePlayerAwards
-from faction.models import Faction
-# from chain.models import Faction
-from awards.models import AwardsData
 # from target.functions import getTargets
-from company.models import CompanyDescription
-from company.models import Company
+from company.models import Company, CompanyDescription
+from faction.models import Faction
+from yata.handy import apiCall, tsnow
+
+# from chain.models import Faction
+
 
 def updatePlayer(player, i=None, n=None):
-    """ update player information
-
-    """
+    """update player information"""
 
     progress = "{:04}/{:04}: ".format(i, n) if i is not None else ""
 
     # personal stats categories
     # Attacking, Jobs, Trading, Jail, Hospital, Finishing Hits, Communication, Criminal Offenses, Bounties, Items, Travel, Drugs, Missions, Racing, Networth and Other.
-    
+
     if player.tId == -1:
         # print("[player.functions.updatePlayer] {}{} action: ignore".format(progress, player.nameAligned()))
         return 0
@@ -47,28 +42,9 @@ def updatePlayer(player, i=None, n=None):
     player.updateKeyLevel()
 
     if player.key_level == 1:
-        selection = [
-            "personalstats",
-            "profile",
-            "discord"
-        ]
+        selection = ["personalstats", "profile", "discord"]
     elif player.key_level == 2:
-        selection = [
-            "personalstats",
-            "crimes",
-            "education",
-            "workstats",
-            "perks",
-            "gym",
-            "merits",
-            "profile",
-            "medals",
-            "honors",
-            "icons",
-            "bars",
-            "discord",
-            "weaponexp"
-        ]
+        selection = ["personalstats", "crimes", "education", "workstats", "perks", "gym", "merits", "profile", "medals", "honors", "icons", "bars", "discord", "weaponexp"]
     elif player.key_level >= 3:
         selection = [
             "personalstats",
@@ -87,29 +63,29 @@ def updatePlayer(player, i=None, n=None):
             "bars",
             "discord",
             "weaponexp",
-            "hof"
+            "hof",
         ]
     else:
         selection = []
-
-    user = apiCall('user', '', ','.join(selection), player.getKey(), kv={"cat": "all"}, verbose=True)
+    # TODO: update to v2 api call
+    user = apiCall("user", "", "".join(selection), player.getKey(), kv={"cat": "all"}, verbose=True)
 
     # set active
     player.active = int(timezone.now().timestamp()) - player.lastActionTS < 60 * 60 * 24 * 31
 
     # change to false if error code 2
-    player.validKey = False if user.get('apiErrorCode', 0) in [1, 2, 10] else player.validKey
+    player.validKey = False if user.get("apiErrorCode", 0) in [1, 2, 10] else player.validKey
 
     # change to true if fetch result
-    player.validKey = True if user.get('name', False) else player.validKey
+    player.validKey = True if user.get("name", False) else player.validKey
 
     # delete key if not valid
     if not player.validKey:
         player.key_set.all().delete()
 
     # discrod id
-    dId = user.get('discord', {'discordID': ''})['discordID']
-    player.dId = 0 if dId in [''] else dId
+    dId = user.get("discord", {"discordID": ""})["discordID"]
+    player.dId = 0 if dId in [""] else dId
 
     # skip if not yata active and no valid key
     if not player.active and not player.validKey:
@@ -119,8 +95,12 @@ def updatePlayer(player, i=None, n=None):
         player.save()
         return 0
 
-    elif 'apiError' in user: # skip if api error (not invalid key)
-        print("[player.functions.updatePlayer] {}{} action: {:010} active: {:1} api: {:1} -> api error {}".format(progress, player.nameAligned(), player.lastActionTS, player.active, player.validKey, user["apiError"]))
+    elif "apiError" in user:  # skip if api error (not invalid key)
+        print(
+            "[player.functions.updatePlayer] {}{} action: {:010} active: {:1} api: {:1} -> api error {}".format(
+                progress, player.nameAligned(), player.lastActionTS, player.active, player.validKey, user["apiError"]
+            )
+        )
         player.key_last_code = user["apiErrorCode"]
         player.save()
         return 0
@@ -142,8 +122,8 @@ def updatePlayer(player, i=None, n=None):
         if faction is None:
             faction = Faction.objects.create(tId=player.factionId)
         faction.name = player.factionNa
-
-        if 'money' in apiCall('faction', '', 'currency', player.getKey()):
+        # TODO: update to v2
+        if "money" in apiCall("faction", "", "currency", player.getKey()):
             player.factionAA = True
             player.chainInfo = "{}".format(player.factionNa)
         else:
