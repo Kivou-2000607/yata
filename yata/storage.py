9fab61fbf9d18afec05c30917548786ec7791956
Decap [47662] <you@example.com>
2026-02-27 09:02:34 +0000

    Storage

--- yata/storage.py
+++ yata/storage.py
@@ -3,3 +3,14 @@ from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 
 class ManifestStaticFilesStorageNotStrict(ManifestStaticFilesStorage):
     manifest_strict = False
+
+    def url_converter(self, name, hashed_files, template=None):
+        converter = super().url_converter(name, hashed_files, template)
+
+        def safe_converter(matchobj):
+            try:
+                return converter(matchobj)
+            except ValueError:
+                return matchobj.group(0)
+
+        return safe_converter
